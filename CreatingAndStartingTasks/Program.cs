edf206ba20285494b46bff1833b75be98654aacf
galaxy <galaxymacos@gmail.com>
2018-07-03 23:49:16 -0400

    + Creating and starting tasks

--- CreatingAndStartingTasks/Program.cs
+++ CreatingAndStartingTasks/Program.cs
@@ -6,10 +6,32 @@ using System.Threading.Tasks;
 
 namespace CreatingAndStartingTasks
 {
-    class Program
+    internal static class Program
     {
-        static void Main(string[] args)
+        public static void Write(object o)
         {
+            int i = 1000;
+            while (i-- > 0)
+            {
+                Console.Write(o);
+            }
+        }
+
+        public static int TextLength(object o)
+        {
+            Console.WriteLine($"\nTask with id {Task.CurrentId} processing object {o}...");
+            return o.ToString().Length;
+        }
+        
+        private static void Main(string[] args)
+        {
+            string text1 = "testing", text2 = "this";
+            var task1 = new Task<int>(TextLength, text1);
+            task1.Start();
+            Task<int> task2 = Task.Factory.StartNew(TextLength, text2);
+            Console.WriteLine($"Length of {text1} is {task1.Result}");
+            Console.WriteLine($"Length of {text2} is {task2.Result}");
+            
         }
     }
 }
